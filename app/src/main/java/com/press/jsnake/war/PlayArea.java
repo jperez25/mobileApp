58ae067c024abf476c70d65c23b2fe62fe54c252
jperez25 <jova9991@gmail.com>
2018-04-02 11:07:49 -0500

    basic layout

---
+++ app/src/main/java/com/press/jsnake/war/PlayArea.java
@@ -0,0 +1,25 @@
+package com.press.jsnake.war;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlayArea extends Fragment {
+
+
+    public PlayArea() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_play_area, container, false);
+    }
+
+}
