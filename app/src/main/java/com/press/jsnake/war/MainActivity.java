34a3902e384999dcaaf91c65d490503ee68a141c
jperez25 <jova9991@gmail.com>
2018-04-02 10:16:31 -0500

    basic files

--- app/src/main/java/com/press/jsnake/war/MainActivity.java
+++ app/src/main/java/com/press/jsnake/war/MainActivity.java
@@ -11,8 +11,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //dwfsd
-    //dsfsdf
-    //sdfsd
-    //sdfsdf
+    //xxada
 }
